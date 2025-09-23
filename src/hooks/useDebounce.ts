99844848d69c7d0b51bd567f4d48447a0611cdd6
Arianna <ariigrangetto5@gmail.com>
2025-09-23 19:23:03 -0300

    fixing useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,6 @@
 import { useCallback, useRef } from "react";
 import { useTranslate } from "./useTranslate.ts";
+import type { LanguageKeys } from "../types";
 
 export default function useDebounce() {
   const timeoutId = useRef<number>(0);
@@ -9,7 +10,11 @@ export default function useDebounce() {
     (input: string, sourceLanguage: string, targetLanguage: string) => {
       clearTimeout(timeoutId.current);
       timeoutId.current = setTimeout(() => {
-        translate(input, sourceLanguage, targetLanguage);
+        translate(
+          input,
+          sourceLanguage as LanguageKeys,
+          targetLanguage as LanguageKeys
+        );
       }, 500);
     },
     [translate]
